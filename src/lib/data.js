67743bca4dd503d46d21d4891c21e85716a29117
0xGu4rdI4N <guardian1265@gmail.com>
2025-12-13 11:46:47 +0530

    Add TOPO-ESA publication

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,4 +1,10 @@
-export const PUBLICATIONS = [];
+export const PUBLICATIONS = [
+    {
+        title: "TOPO-ESA: Topology-Driven EEG Signal Analysis for Cognitive Workload Recognition",
+        venue: "Submitted to ICASSP 2026 (under peer review)",
+        desc: "K. Gaurav, N. Sharma, R. Kumar, T. K. Reddy Bollu"
+    }
+];
 
 export const TIMELINE_ITEMS = [];
 
